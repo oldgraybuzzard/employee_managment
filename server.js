1b5900ad15f97fe884031239bd5a01dbced02ac4
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-14 06:07:32 -0500

    comments added to areas for future development

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { prompt } = require('inquirer');
 const express = require('express');
 const cTable = require('console.table');
 const db = require('./db/connection');
+//api route for future development
 const apiRoutes = require('./routes');
 const init = require('./utils/prompts');
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 -
-// Use apiRoutes
+// Use apiRoutes - for future development
 app.use('/api', apiRoutes);
 
 db.connect(err => {
